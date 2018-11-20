d7c0094eac7265eeeb636ce82c05171b50f9c7aa
Calberk <park.edmund@gmail.com>
2018-11-20 15:19:40 -0800

    completed add item to server functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,12 +5,21 @@ import axios from 'axios';
 const BASE_URL = 'http://api.reactprototypes.com/todos';
 const API_KEY = '?key=c918_edmund';
 
+export function addTodoItem(item){
+    const resp = axios.post(BASE_URL + API_KEY, item);
+
+    return {
+        type: types.ADD_LIST_ITEM,
+        payload: resp,
+    }
+}
+
 export function getListData(){
 
     const resp = axios.get(BASE_URL + API_KEY);
 
     return {
         type: types.GET_ALL_LIST_DATA,
-        payload: resp
+        payload: resp,
     }
 }
